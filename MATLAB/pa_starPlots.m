5b468fa547d6c7c2354227c43e59ee16adaeb2bc
rf-engine <jmlee22@vt.edu>
2026-03-26 13:53:56 -0400

    Updates Files

--- MATLAB/pa_starPlots.m
+++
@@ -1,7 +0,0 @@
-clear variables; close all; clc;
-
-dataFolder = 'G:\My Drive\Publications\SSCS Magazine\Fall 2025\data\';
-survey = 'PA-Survey-v10.xlsx';
-dataPath = cat(2,dataFolder,survey);
-
-cmos_data = readtable(dataPath, 'Sheet', 'CMOS');
